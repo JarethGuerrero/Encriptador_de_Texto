1cd6499732285e6945517fd92cf247dfe3c0f4bc
JarethGuerrero <yaredguerrero@gmail.com>
2023-07-14 19:59:14 -0600

    Comenzando el js

---
+++ script.js
@@ -0,0 +1,5 @@
+var btnEncriptar = document.querySelector("[data-btn-encriptar]")
+var btnDesencriptar = document.querySelector("[data-btn-desencriptar]")
+
+var btnCopiar = document.querySelector("[data-btn-copiar]")
+//var textoEntrada = document.querySelector("[data-texto-entrada]")
