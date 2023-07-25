b4ecf9021bbfc59a63e66583d94e9744f2b5d3d0
JarethGuerrero <yaredguerrero@gmail.com>
2023-07-24 19:22:57 -0600

    Creando las variables para encriptar/desencriptar.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-var btnEncriptar = document.querySelector("[data-btn-encriptar]")
-var btnDesencriptar = document.querySelector("[data-btn-desencriptar]")
 
-var btnCopiar = document.querySelector("[data-btn-copiar]")
-//var textoEntrada = document.querySelector("[data-texto-entrada]")
+
+function encriptar(){
+    
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replaceAll("e","enter")
+                               .replaceAll("i","imes")
+                               .replaceAll("a","ai")
+                               .replaceAll("o","ober")
+                               .replaceAll("u","ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;                           
+};
+
+function desencriptar(){
+    
+    var frase = document.getElementById("textoEncriptado").value;
+
+    var textoEncriptado = frase.replaceAll("enter","e")
+                               .replaceAll("imes","i")
+                               .replaceAll("ai","a")
+                               .replaceAll("ober","o")
+                               .replaceAll("ufat","u");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;                           
+}
+document.getElementById("btnEncriptar").addEventListener("click", encriptar);
+document.getElementById("btnDesencriptar").addEventListener("click", desencriptar);
